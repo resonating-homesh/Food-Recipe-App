c7bef67314f69158615b7c4f34631b2675af6b18
Homesh <homeshnimbarte2@gmail.com>
2022-05-23 15:39:53 +0530

    improvised and added comments

--- src/App.js
+++ src/App.js
@@ -1,110 +1,123 @@
 import './App.css';
 import axios from "axios";
-import { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Grid from './Grid';
 import Spinner from './Spinner';
 
 function App() {
-  const [loading, setLoading] = useState(false);
-  const [query, setQuery] = useState("");
-  const [recipes, setRecipes] = useState([]);
-  const [pre, setPre] = useState(true);
-  const [test, setTest] = useState(false);
+  const [loading, setLoading] = useState(false); //determines whether the page is loading 
+  const [query, setQuery] = useState(""); //stores the data put in by user to search
+  const [recipes, setRecipes] = useState([]); //stores in the data recieved from axios request
+  const [pre, setPre] = useState(true);//helps in determining class 
+  const [test, setTest] = useState(false); //helps inn determining class
+  const [length, setLength] = useState(20); //stores the length of the recipes to be received
+  let result; //stores the response from axios request
 
-  const apiKey = process.env.REACT_APP_APIKEY;
+
+  const apiKey = process.env.REACT_APP_APIKEY; //stores api key
 
   const options = {
     method: 'GET',
     url: 'https://tasty.p.rapidapi.com/recipes/list',
-    params: {from: '0',
-    size: '20',
-    q: query},
+    params: {
+      from: length - 20,
+      size: 20,
+      q: query
+    },
     headers: {
       'X-RapidAPI-Host': 'tasty.p.rapidapi.com',
       'X-RapidAPI-Key': apiKey,
     }
   }
-  
-  // axios.request(options).then(function (response) {
-  //   console.log(response);
-  // }).catch(function (error) {
-  //   console.error(error);
-  // });
-
-  async function getRecipes(){
-    setPre(false);
+//function for axios request
+  async function getRecipes() {
+    setPre(false); 
     setLoading(true);
-    const result = await axios.request(options);
+    await axios.request(options).then(function (response) {
+      result = response;
+    }).catch(function (error) {
+      alert(error + "! Try Reloading the app !")
+    })
     console.log(result.data);
-    setRecipes(result.data.results); 
-    if (result.data.results.length === 0)
-    {
+    setRecipes(result.data.results);
+    //if else statement below helps us to determine the classes for the rendered element
+    if (result.data.results.length === 0) {
       setTest(true);
-      console.log(recipes);
+      setPre(true);
     }
-    else
-    {
+    else {
       setTest(false);
-      console.log(recipes);
+      setPre(false);
     }
     setLoading(false);
   }
 
-  const submit = (e) =>
-  {
+  //it will trigger the axios request once we click search btn
+  const submit = (e) => {
     e.preventDefault();
+    setLength(20);
     getRecipes();
   }
 
- function checkClass (loading, pre)
- {
-    if (loading  === false)
-    {
-      if (pre === true)
-      {
-        return "";
+  //determines the class of the rendered div
+  function checkClass2(loading, pre) {
+    if (loading === false) {
+      if (pre === true) {
+        return "none";
       }
-      else{
-        return "grid"
+      else {
+        return "btnSection"
       }
     }
-    else{
-      return "loader";
+    else {
+      return "none";
     }
   }
 
+  //works when triggered by clicking next button
+  function handlePrevClick() {
+    if (length > 20) {
+      setLength(length - 20);
+      console.log(length);
+    }
+  }
+
+  //works when triggered by clicking prev button
+  function handleNextClick() {
+    setLength(length + 20);
+    console.log(length);
+  }
+
+//whenever length's state is changed it gets triggered
+  useEffect(() => {
+    getRecipes();
+    console.log("effect");
+  }, [length])
 
   return (
     <div className="app">
-    <div className='header'>
-      <h1 className='title' >Get Your Recipe !</h1>
-      <form className='form' onSubmit={submit}>
-    <input className='searchInput' type='text' placeholder='Happy Cravings!' value = {query} onChange={(e)=> setQuery(e.target.value)}/>
-    <input className='btn' type="submit" value="Search"/>
-      </form>
+      <div className='header'>
+        <h1 className='title' >Get Your Recipe !</h1>
+        <form className='form' onSubmit={submit}>
+          <input className='searchInput' type='text' placeholder='Happy Cravings!' value={query} onChange={(e) => setQuery(e.target.value)} />
+          <input className='btn' type="submit" value="Search" />
+        </form>
       </div>
-      <div className={checkClass(loading, pre)}>
-        {loading ? <Spinner/> : recipes.map((e)=> {
-          return <Grid e={e}/>;
+      <div className={loading ? "loader" : "grid"}>
+        {loading ? <Spinner /> : recipes.map((e) => {
+          return <Grid e={e} />;
         })}
       </div>
-      <div className={pre ? "null" : "none"}>
-      <div className={test ? "none" : ""}>
-        <p className='nullText'>Looking for a new recipe?</p>
-        <p className='nullText'>Search the one who satisfies your cravings and ENJOY !</p>
-      </div>
-        <img className='nullImage' src='2.jpeg'></img>
+      <div className={checkClass2(loading, pre)}>
+        <button type='button' className='Btn' onClick={handlePrevClick} >Previous</button>
+        <h2 style={{ color: 'white' }} > Page {length / 20} </h2>
+        <button type='button' className='Btn' onClick={handleNextClick} >Next</button>
       </div>
       <div className={test ? "null" : "none"}>
-      <p className={loading ? "nullText" : "none"}> Searching... </p>
-      <p className={loading ? "none" : "nullText"}>Sorry! No Results !</p>
+        <p className={loading ? "nullText" : "none"}> Searching... </p>
+        <p className={loading ? "none" : "nullText"}>Sorry! No Results !</p>
       </div>
     </div>
   );
 }
-
 export default App;
-
-
-//no results found ka kya kre
-
--- src/Grid.js
+++ src/Grid.js
@@ -4,17 +4,13 @@ import "./grid.css";
 export default function Grid({ e }) {
     return (
         <div className='card'>
-            {/* <p className='cardName'> {e["name"]} </p>
-            <img className='cardImage' src={e['thumbnail_url']} alt={e['thumbnail_alt_text']} />
-            <p className='cardLoc'>Created At: {e["created_at"]} </p> */}
             <ul class="card-list">
                 <li>
                     <article>
-                        <h1>{e["name"] ? e["name"].slice(0, 27) : "" }...</h1>
+                        <h1>{e["name"] ? e["name"].slice(0, 27) : ""}...</h1>
                         <section>
-                            <img src={e['thumbnail_url']} alt={e['thumbnail_alt_text']} className="cardImage"/> 
+                            <img src={e['thumbnail_url']} alt={e['thumbnail_alt_text']} className="cardImage" />
                             <div class="content">
-                                {/* <button class="view-recipe">View Recipe</button> */}
                                 <h3> {e['name']} </h3>
                                 <p> {e['description']} </p>
                             </div>
